9b26ddc467b844fe6164894ef014335967b44163
markf <markfitz815@gmail.com>
2023-05-09 21:18:45 +0100

    Add player and physics collision

--- src/index.js
+++ src/index.js
@@ -22,8 +22,10 @@ class MyGame extends Phaser.Scene {
   }
 
   create() {
+    // Create sky
     this.add.image(400, 300, "sky");
 
+    // Create platforms
     const platforms = this.physics.add.staticGroup();
 
     platforms.create(400, 568, "ground").setScale(2).refreshBody();
@@ -31,14 +33,43 @@ class MyGame extends Phaser.Scene {
     platforms.create(600, 400, "ground");
     platforms.create(50, 250, "ground");
     platforms.create(750, 220, "ground");
+
+    // Player Creation
+    const player = this.physics.add.sprite(100, 450, "dude");
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    // Add physics collision
+    this.physics.add.collider(player, platforms);
   }
 }
 
 const config = {
   type: Phaser.AUTO,
   parent: "Mark Game",
-  width: 1080,
-  height: 720,
+  width: 800,
+  height: 600,
   physics: {
     default: "arcade",
     arcade: {
